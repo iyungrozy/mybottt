b5ee356fc103fd2244e963694674907c955649b9
iyungrozy <36526260+iyungrozy@users.noreply.github.com>
2023-02-21 20:43:07 +0700

    Update help.js

--- Commands/Core/help.js
+++ Commands/Core/help.js
@@ -29,7 +29,7 @@ if (args[0]) {
 
 let textHelpMenu = `Konnichiwa *${pushName}* Senpai,
 
-I am *${botName}*, a bot developed by *Team Atlas*.
+I am *${botName}*, a bot developed by Honjoo.
 
 🎀 My prefix is: *${prefix}*
 
@@ -245,7 +245,7 @@ textHelpMenu += `╭────ꕥ NSFW ꕥ────╮
 }
           
 textHelpMenu +=`*🔰  ${botName}  🔰*
- _Powered By:_ *Team ATLAS*
+ _Powered By:_ HONJO
 🎀 To use any of these commands type 
 " *${prefix}Command name* ".
 🏮 To get Support Group link type " *${prefix}support* ".
